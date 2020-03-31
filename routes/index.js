cc4ab6893da785dbf46f3c27d44c175b354db575
mazharhasan <smazherhq@gmail.com>
2020-03-30 23:52:02 -0400

    Index modified

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Home page requested.");
   res.render('index', { title: 'Express' });
 });
 
